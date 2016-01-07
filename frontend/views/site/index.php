79f538a10ea101e2f29a61d3f0586cf8a237d5a5
antastic <ryuofgod@gmail.com>
2016-01-07 21:24:43 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,7 +1,21 @@
 <?php
-
+use yii\helpers\Html;
+use yii\helpers\Url;
 /* @var $this yii\web\View */
 
 $this->title = 'ระบบจัดการแฟต รพ.สารภี';
 ?>
-
+<?php
+    $route1=Yii::$app->urlManager->createUrl('test/test1');
+?>
+<a href="<?=$route1?>">ไปที test1</a>
+<br>
+<?php
+    $route2=Yii::$app->urlManager->createUrl(['test/test2','name'=>'อมรเทพ','lname'=>"สุวรรณคำ"]);
+?>
+<a href="<?=$route2?>">ไปที test2</a>
+<br>
+<?=
+        Html::a('ลิงค์แบบที3',['test/test1']);
+        ?>
+<br>
